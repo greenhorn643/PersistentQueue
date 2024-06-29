59e362730f06e22314d9f7b5857c2d80e8b6de12
alex.palmer643 <alex.palmer643@gmail.com>
2024-06-30 04:36:26 +1200

    Exposed non-waiting version of TryPeekFile in IPersistentQueue

--- Persistify/IPersistentQueue.cs
+++ Persistify/IPersistentQueue.cs
@@ -3,6 +3,7 @@
 public interface IPersistentQueue<TItem> : IDisposable
 {
 	void Enqueue(TItem item);
+	bool TryPeekFile(out List<TItem>? items);
 	bool TryPeekFile(out List<TItem>? items, TimeSpan timeout);
 	void PopFile();
 	void ReplaceFrontFile(List<TItem> items);
